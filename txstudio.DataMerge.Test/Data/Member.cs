239786e06dac1e5358d4056d8cb23bff0c689a6c
Shi Hao Wang <st890409@hotmail.com>
2018-09-29 16:21:23 +0800

    建立測試專案與預設內容

---
+++ txstudio.DataMerge.Test/Data/Member.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace txstudio.DataMerge.Test
+{
+    public class Member : IKeyEquals
+    {
+        public int? No { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public bool KeyEquals(object obj)
+        {
+            if (obj is Member)
+            {
+                var _member = obj as Member;
+
+                if (int.Equals(this.No, _member.No) == false)
+                    return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Member)
+            {
+                var _comparison = StringComparison.OrdinalIgnoreCase;
+                var _member = obj as Member;
+
+                if (int.Equals(this.No, _member.No) == false)
+                    return false;
+
+                if (string.Equals(this.Email, _member.Email, _comparison) == false)
+                    return false;
+
+                if (string.Equals(this.Name, _member.Name, _comparison) == false)
+                    return false;
+
+                if (DateTime.Equals(this.Birthday, _member.Birthday) == false)
+                    return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(No, Email, Name, Birthday);
+        }
+    }
+}
---
+++ txstudio.DataMerge.Test/MergeDataGenerater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace txstudio.DataMerge.Test
+{
+    public sealed class MergeDataGenerater
+    {
+        public static IEnumerable<Member> GetTargets()
+        {
+            List<Member> _target = new List<Member>();
+
+            _target.Add(new Member() { No = 1, Name = "Lucas Yang", Birthday = new DateTime(1984, 2, 13), Email = "lucas@txstudio.com" });
+            _target.Add(new Member() { No = 2, Name = "Peter Chang", Birthday = new DateTime(1975, 10, 29), Email = "peter@txstudio.com" });
+            _target.Add(new Member() { No = 3, Name = "Will Pao", Birthday = new DateTime(1988, 2, 20), Email = "will@txstudio.com" });
+            _target.Add(new Member() { No = 6, Name = "Wei Chen", Birthday = new DateTime(1992, 1, 3), Email = "wei@txstudio.com" });
+            
+            return _target;
+        }
+
+        public static IEnumerable<Member> GetSources()
+        {
+            List<Member> _source = new List<Member>();
+
+            _source.Add(new Member() { No = 2, Name = "Peter Chang", Birthday = new DateTime(1975, 10, 29), Email = "peter@txstudio.tw" });
+            _source.Add(new Member() { No = 3, Name = "Will Pao", Birthday = new DateTime(1988, 2, 29), Email = "will@txstudio.com" });
+            _source.Add(new Member() { No = 4, Name = "Lewis Wang", Birthday = new DateTime(1990, 9, 3), Email = "lewis@txstudio.com" });
+            _source.Add(new Member() { No = 5, Name = "Lucy Joy", Birthday = new DateTime(1991, 12, 25), Email = "lucy@txstudio.com" });
+            _source.Add(new Member() { No = 6, Name = "Wei Chen", Birthday = new DateTime(1992, 1, 3), Email = "wei@txstudio.com" });
+
+            return _source;
+        }
+    }
+}
